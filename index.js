0c1e4b3848b91d0f5f001df447ef2edc28a3ab3c
Armand SONGOTI <armand.songoti@neemba.com>
2025-08-21 16:02:57 +0000

    first bubble  game commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let result = document.querySelector('.result');
+
+// setInterval(() => {
+//     const bulle = document.createElement('span');
+//     bulle.classList.add('bulle');
+//     let size =  Math.random() * 100 + 50 + 'px';
+//     bulle.style.left = Math.random() * 100 + '%';
+//     bulle.style.height = size;
+//     bulle.style.width = size;
+//     bulle.style.setProperty('--left', Math.random() * 100 + '%');
+//     document.body.appendChild(bulle);
+//     bulle.addEventListener('click', () => {
+//           bulle.remove();
+//        result.textContent = parseInt(result.textContent) + 1;
+//     });
+//     setTimeout(() => {
+//         bulle.remove();
+//     }, 3000);
+// }, 200);
+
